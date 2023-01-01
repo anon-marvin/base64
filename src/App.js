597c8bca415602e6c7750cf8700bef0c6dfa8f07
anon-marvin <anon.marvin.sohan@gmail.com>
2023-01-01 19:54:31 +0530

    dfgd
    sfswad

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 
+
 function App() {
-  return (
-    <>
-      <Navbar></Navbar>
-    </>
-  );
+	return (
+		<>
+			<Navbar></Navbar>
+
+
+		</>
+	);
 }
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,33 +3,34 @@ import React from 'react'
 export default function Navbar() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-dark bg-primary ">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Navbar</a>
+                    <a className="navbar-brand  text-white" href="/">Navbar</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                <a className="nav-link active text-white" aria-current="page" href="/"> <i class="fa-solid fa-house"></i>Home</a>
+
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Link</a>
+                                <a className="nav-link text-white" href="/"><i class="fa-solid fa-address-book"></i>Contact</a>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a className="nav-link dropdown-toggle text-white" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-screwdriver-wrench"></i>
                                     Other Tools
                                 </a>
-                                <ul className="dropdown-menu">
+                                <ul className="dropdown-menu w-70">
                                     <li><a className="dropdown-item" href="/">Action</a></li>
                                     <li><a className="dropdown-item" href="/">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="/">Something else here</a></li>
+
+
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">Info</a>
+                                <a className="nav-link text-white" href="/"><i class="fa-solid fa-info"></i>Info</a>
                             </li>
 
                         </ul>
